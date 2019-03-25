bbdd81181dce9db8f9bde9adacdf793990900437
Kole Tackney <kole.tackney@youi.tv>
2019-03-25 13:13:10 -0400

    start of chess piece and board.
    started making the chess pieces, and basic movement. not finnished yet.

---
+++ Assets/Script/ChessPieces/ChessPiece.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ChessPiece : MonoBehaviour
+{   
+    protected Vector2Int m_PlayerPos;
+    protected int m_PlayerOrientation; // determines chess forward vector
+
+    protected ChessBoard chessBoard = ChessBoard.getInstance();
+
+    public List<Vector2Int> m_MovementOffsets;
+    public virtual List<Vector2Int> getAvailableMoves()
+    {
+         List<Vector2Int> availMoves = new List<Vector2Int>();
+         
+         foreach(Vector2Int squareToCheck in m_MovementOffsets)
+        {
+            if(chessBoard.isSquareAvailable(squareToCheck, this))
+            {
+                availMoves.Add(squareToCheck);
+            }
+        }        
+        return availMoves;
+    }
+
+    public virtual void movePiece(Vector2Int newPos)
+    {
+        //Check to see if another piece is already on the square in question. 
+
+        //Move piece to new location.
+
+    }
+
+    public virtual bool KillPiece()
+    {
+        //Play attack animation 
+
+        return false; 
+    }
+
+    public int getOrientation()
+    {
+        return m_PlayerOrientation;
+    }
+}
