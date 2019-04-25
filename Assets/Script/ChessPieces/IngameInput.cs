473d118280b827a4217258cb399cb2be7aad84c9
Xioor <erb365@hotmail.com>
2019-04-25 14:09:54 -0400

    Add ability to touch chess piece

---
+++ Assets/Script/ChessPieces/IngameInput.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngameInput : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("Object touched");
+            }
+        }
+    }
+}
