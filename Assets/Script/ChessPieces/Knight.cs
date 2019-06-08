39b62b20f016625912d6334e1523416e90999687
Xioor <erb365@hotmail.com>
2019-06-08 17:12:46 -0400

    Adding knight

--- Assets/Script/ChessPieces/Knight.cs
+++ Assets/Script/ChessPieces/Knight.cs
@@ -9,4 +9,20 @@ public class Knight : ChessPiece
         return PieceType.Knight;
     }
 
+    public override List<Vector2Int> getAvailableMoves(List<Vector2Int> squaresOverride = default(List<Vector2Int>), PieceMoveRestriction pieceMoveRestriction = PieceMoveRestriction.OnlyWhenPositionFreeOrOccupiedByOpponent, bool bCheckSameColor = false)
+    {
+        List<Vector2Int> availableMoves = new List<Vector2Int>();
+
+        TryAddingAvailableMove(ref availableMoves, 2, 1, pieceMoveRestriction, bCheckSameColor);
+        TryAddingAvailableMove(ref availableMoves, 2, -1, pieceMoveRestriction, bCheckSameColor);
+        TryAddingAvailableMove(ref availableMoves, 1, 2, pieceMoveRestriction, bCheckSameColor);
+        TryAddingAvailableMove(ref availableMoves, 1, -2, pieceMoveRestriction, bCheckSameColor);
+
+        TryAddingAvailableMove(ref availableMoves, -2, -1, pieceMoveRestriction, bCheckSameColor);
+        TryAddingAvailableMove(ref availableMoves, -2, 1, pieceMoveRestriction, bCheckSameColor);
+        TryAddingAvailableMove(ref availableMoves, -1, -2, pieceMoveRestriction, bCheckSameColor);
+        TryAddingAvailableMove(ref availableMoves, -1, 2, pieceMoveRestriction, bCheckSameColor);
+
+        return availableMoves;
+    }
 }
